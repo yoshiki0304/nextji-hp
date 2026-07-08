7e558f4516b571b2b015a759fd250d5307865278
yoshiki0304 <108782446+yoshiki0304@users.noreply.github.com>
2026-07-08 18:28:09 +0900

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+if (menuBtn && nav) {
+  menuBtn.addEventListener('click', () => nav.classList.toggle('open'));
+}
